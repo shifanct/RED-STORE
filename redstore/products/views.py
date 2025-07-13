780a8a84a7705e9b0515ca5903e81d2dddf1bd27
muhammed <muhammedshifan475@gmail.com>
2025-07-13 21:12:14 +0530

    Added next page option on products page.

--- redstore/products/views.py
+++ redstore/products/views.py
@@ -7,18 +7,36 @@ from django.contrib.auth.decorators import login_required
 @login_required(login_url='/login')
 def product_list(request):
     all_products = Products.objects.all()
-    paginator = Paginator(all_products, 8)  
-    page_number = request.GET.get('page') 
-    page_products = paginator.get_page(page_number)
-    num_of_pages = paginator.num_pages
-    dict ={'pages':[]}
-    lst = []
-    for i in range(1,num_of_pages+1):
-        lst.append(i)
-    dict['pages'] = lst
-    print(dict)
+    paginator = Paginator(all_products, 8)
 
-    return render(request, 'products.html', {'products': page_products, 'pages':dict['pages']})
+    page_param = request.GET.get('page')
+    current_page = request.GET.get('current', '1')
+
+    try:
+        current_page = int(current_page)
+    except ValueError:
+        current_page = 1
+
+    if page_param == 'next_page':
+        page_number = current_page + 1
+    else:
+        try:
+            page_number = int(page_param)
+        except (TypeError, ValueError):
+            page_number = 1
+
+    page_products = paginator.get_page(page_number)
+
+    num_of_pages = paginator.num_pages
+    pages_list = list(range(1, num_of_pages + 1))
+
+    return render(request, 'products.html', {
+        'products': page_products,
+        'pages': pages_list,
+        'current_page': page_products.number
+    })
+
+
 
 @login_required(login_url='/login')
 def product_detailed_view(request, pk):
